e39f2d7f3fb47094c8992861b445a6e75215d337
YuriiKhomich <123492828+YuriiKhomich@users.noreply.github.com>
2023-05-31 16:29:23 +0300

    структура проекта

---
+++ main.py
@@ -0,0 +1,96 @@
+# Словарь для хранения пользователей
+users = {}
+
+# Словарь для хранения твитов
+tweets = {}
+
+# Словарь для хранения комментариев к твитам
+comments = {}
+
+# Словарь для хранения оценок твитов
+ratings = {}
+
+
+def register():
+    # Регистрация нового пользователя
+    pass
+
+
+def login():
+    # Вход в систему
+    pass
+
+
+def main_menu(username):
+    # Основное меню пользователя
+    pass
+
+
+def create_tweet(username):
+    # Создание нового твита
+    pass
+
+
+def view_tweets(username):
+    # Просмотр всех твитов пользователя
+    pass
+
+
+def edit_tweet(username, tweet_id):
+    # Редактирование твита
+    pass
+
+
+def delete_tweet(username, tweet_id):
+    # Удаление твита
+    pass
+
+
+def get_tweet_rating(username, tweet_id):
+    # Получение рейтинга твита
+    pass
+
+
+def explore_accounts(username):
+    # Просмотр других аккаунтов
+    pass
+
+
+def search_users():
+    # Поиск пользователей
+    pass
+
+
+def view_user_tweets(username, target_user):
+    # Просмотр твитов определенного пользователя
+    pass
+
+
+def read_tweet(username, target_user, tweet_id):
+    # Чтение твита определенного пользователя
+    pass
+
+
+def leave_comment(username, target_user, tweet_id, comment):
+    # Оставление комментария к твиту
+    pass
+
+
+def view_comments(username, target_user, tweet_id):
+    # Просмотр комментариев к твиту
+    pass
+
+
+def rate_tweet(username, target_user, tweet_id, rating):
+    # Оценка твита
+    pass
+
+
+def main():
+    # Основная функция программы
+    pass
+
+
+# Запуск программы. Точка входа.
+if __name__ == "__main__":
+    main()
